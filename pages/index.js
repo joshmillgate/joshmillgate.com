42b56d4507ae5c4e2ea6df070254838913250a84
joshmillgate <hello@joshmillgate.co.uk>
2021-06-25 23:58:18 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Home() {
   return (
@@ -11,17 +13,15 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Josh Millgate
+          Purpose-driven design
         </h1>
 
         <p className={styles.description}>
          Creating with an awareness of the bigger picture.
         </p>
+        <code className={styles.code}>Website under construction</code>
+        <a href="https://twitter.com/millgatejosh"><FontAwesomeIcon className={styles.twitter} icon={faTwitter} width="30px" /></a>
       </main>
-
-      <footer className={styles.footer}>
-        2021
-      </footer>
     </div>
   )
 }
